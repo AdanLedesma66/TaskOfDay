505ddc32d8369b0d7954794744a09053453543f6
aledesma <adagalese@gmail.com>
2023-02-22 15:16:17 -0300

    proyecto añadido

---
+++ src/main/java/py/com/task/TaskOfDayApplication.java
@@ -0,0 +1,13 @@
+package py.com.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOfDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskOfDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/py/com/task/TaskOfDayApplicationTests.java
@@ -0,0 +1,13 @@
+package py.com.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskOfDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
